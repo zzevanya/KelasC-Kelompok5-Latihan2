da0587772751c2e018c0d255e92383fa37dcd337
aurreadaffa17 <daffaaurre@gmail.com>
2026-06-15 19:52:31 +0700

    Create perpustakaan.py

---
+++ perpustakaan.py
@@ -0,0 +1,37 @@
+from fitur_tambah import menu_tambah
+from fitur_aksi import menu_hapus, menu_tampil
+
+class Perpustakaan:
+    def __init__(self):
+        self.daftar_koleksi = []
+
+    def wait_enter(self):
+        input("Tekan [ENTER] untuk kembali ke menu program")
+        print("\n" + "="*50 + "\n")
+
+    def run(self):
+        while True:
+            print("-"*50)
+            print("="*18, "MENU PROGRAM", "="*18)
+            print("-"*50)
+            print("  1. Tambah data koleksi")
+            print("  2. Hapus data koleksi")
+            print("  3. Tampil semua data koleksi")
+            print("  4. Keluar\n")
+
+            pilihan = input("Nomor yang dipilih: ")
+
+            if pilihan == '1':
+                menu_tambah(self.daftar_koleksi)
+                self.wait_enter()
+            elif pilihan == '2':
+                menu_hapus(self.daftar_koleksi)
+                self.wait_enter()
+            elif pilihan == '3':
+                menu_tampil(self.daftar_koleksi)
+                self.wait_enter()
+            elif pilihan == '4':
+                print("\nTerima kasih telah menggunakan program perpustakaan.")
+                break
+            else:
+                print("Pilihan tidak valid! Silakan coba lagi.\n")
